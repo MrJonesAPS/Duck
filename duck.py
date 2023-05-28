06c9ba4f40ea92cd61fd5a7c56277aec2a1afcaf
Chris Jones <christopher.jones@apsva.us>
2023-05-27 21:48:00 -0400

    Re-architecting

--- duck.py
+++ duck.py
@@ -5,10 +5,10 @@ import sqlite3
 from datetime import date, datetime, timedelta
 from flask import Flask, redirect, url_for, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
-import board
-import busio
-import adafruit_thermal_printer
-import serial
+#import board
+#import busio
+#import adafruit_thermal_printer
+#import serial
 
 from flask_login import (
     LoginManager,
@@ -20,12 +20,11 @@ from flask_login import (
 )
 from oauthlib.oauth2 import WebApplicationClient
 import requests
-from escpos.printer import Serial
+#from escpos.printer import Serial
 
 #configuration
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-SERVER_IP_ADDRESS = os.environ.get("IP", None).strip()
 ADMIN_USER_NUM = os.environ.get("ADMIN_USER_NUM", None).strip()
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
@@ -36,9 +35,9 @@ app.config.from_pyfile('instance/config.py')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = "home"
 #I don't need to flash a message if logged out because I always just redirect to login
-login_manager.login_message = None
+login_manager.login_message = "You are not logged in"
 
 
 # OAuth 2 client setup
@@ -48,83 +47,6 @@ client = WebApplicationClient(GOOGLE_CLIENT_ID)
 @login_manager.user_loader
 def load_user(user_id):
     return get_user(user_id)
-
-def initializePrinter():
-    ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.68)
-    uart = serial.Serial("/dev/serial0", baudrate=19200, timeout=0)
-    printer = ThermalPrinter(uart, auto_warm_up=False, dot_print_s = 0.01, byte_delay_s = 0)
-    printer.warm_up()
-    printer.print("DUCK is online. Here is the IP address:")
-    printer.print(SERVER_IP_ADDRESS)
-    printer.feed(2)
-    return printer
-
-def initializePrinter_escpos():
-    p = Serial(devfile='/dev/serial0',
-           baudrate=19200,
-           bytesize=8,
-           parity='N',
-           stopbits=1,
-           timeout=1.00,
-           dsrdtr=True)
-    return p
-
-def checkPaper():
-    if printer.has_paper():
-        pass
-    else:
-        flash("The printer is out of paper. Tell Mr. Jones to fix it!","error")
-
-def PrintHallPass(name, destination, date, time, id):
-    printer.size = adafruit_thermal_printer.SIZE_MEDIUM
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.print("__(.)<   THIS IS A   <(.)__")
-    printer.print("\___)    HALL PASS    (___/")
-    printer.feed(1)
-    printer.print(name)
-    printer.print("is going to " + destination)
-    printer.print("at " + time)
-    printer.print("on " + date)
-    printer.feed(1)
-    printer.print("Questions? See Mr. Jones")
-    printer.print("in room B130")
-    printer.feed(1)
-    printer.size = adafruit_thermal_printer.SIZE_SMALL
-    printer.print("(scan to validate)")
-    printer_escpos.qr("http://10.39.13.236/view_pass/" + str(id),native=False ,size=8)
-    printer_escpos.text("\n\n\n")
-
-def PrintWPPass(name, date):
-    ###
-    #I got some of this code from stackoverflow
-    #https://stackoverflow.com/questions/5891555/display-the-date-like-may-5th-using-pythons-strftime
-    ###
-    def suffix(d):
-        return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
-
-    def custom_strftime(format, t):
-        return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
-
-    printer.size = adafruit_thermal_printer.SIZE_LARGE
-    printer.justify = adafruit_thermal_printer.JUSTIFY_LEFT
-    printer.print("__(.)<   WARRIOR")
-    printer.print("\___)    PASS")
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.feed(3)
-    printer.print(name)
-    printer.print("is invited")
-    printer.print("to room B130")
-    printer.print("on")
-    printer.print(custom_strftime('%a, %B {S}', date))
-    printer.feed(3)
-    printer.print("Questions?") 
-    printer.print("See Mr. Jones")
-    printer.print("in room B130")
-    printer.feed(2)
-
-@app.context_processor
-def inject_dict_for_all_templates():
-    return dict(SERVER_IP_ADDRESS=SERVER_IP_ADDRESS)
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
@@ -191,6 +113,7 @@ def callback():
     if userinfo_response.json().get("email_verified"):
         unique_id = userinfo_response.json()["sub"]
         users_name = userinfo_response.json()["given_name"]
+        #return "userid: " + unique_id,400
     else:
         return "User email not available or not verified by Google.", 400
 
@@ -200,10 +123,9 @@ def callback():
 
     # Begin user session by logging the user in
     if user != None:
-        login_user(user)
+        login_user(user, remember=True)
     else:
-        flash("You just tried to login with an unknown userid: " + user.id,"error")
-
+        return ("You just tried to login with an unknown userid: " + unique_id), 400
     # Send user back to homepage
     return redirect(url_for('pass_admin'))
 
@@ -224,7 +146,6 @@ def logout():
 def helpQ():
     if request.method == "GET":
         waiter_list = Waiter.query.all()
-        checkPaper()
         return render_template("helpQ.html", waiter_list = waiter_list, current_user=current_user)   
 
 
@@ -248,7 +169,7 @@ def pass_admin():
                                                       WPPass.rejected.is_(False))).scalars()
 
 
-        checkPaper()
+        
         
         #####
         #When a student has an unapproved request, quack
@@ -280,7 +201,7 @@ def approve_pass(id):
     db.session.commit()
     nowTime = datetime.now().strftime("%I:%M %p")
     nowDate = date.today().strftime("%B %d, %Y")
-    PrintHallPass(thisPass.name, thisPass.destination, nowDate, nowTime, id)
+    #PrintHallPass(thisPass.name, thisPass.destination, nowDate, nowTime, id)
     return redirect(url_for("pass_admin"))  
 
 @app.route("/reject_pass/<id>", methods=["GET"])
@@ -300,7 +221,7 @@ def approve_wp(id):
     print(thisPass)
     thisPass.approved_datetime = datetime.now()
     db.session.commit()
-    PrintWPPass(thisPass.name, thisPass.date)
+    #PrintWPPass(thisPass.name, thisPass.date)
     return redirect(url_for("pass_admin"))  
 
 @app.route("/reject_wp/<id>", methods=["GET"])
@@ -324,7 +245,6 @@ def return_pass(id):
 
 @app.route("/view_pass/<id>", methods=["GET"])
 def view_pass(id):
-    
     
     thisPass = db.session.execute(db.select(HallPass).filter_by(id=id)).scalar_one_or_none()
     
@@ -356,9 +276,24 @@ def view_pass(id):
 
 @app.route("/request_pass", methods=["GET","POST"])
 def request_pass():
+    if request.method == "GET":
+        #checkPaper()
+        return render_template("request_pass.html")
+    elif request.method == "POST":
+        name = request.form.get("name")
+        destination = request.form.get("destination")
+        request_datetime = datetime.now()
+        new_pass_request = HallPass(name=name, destination=destination,request_datetime=request_datetime,rejected=False)
+        db.session.add(new_pass_request)
+        db.session.commit()
+        flash("Hi " + name + " your pass for " + destination + " has been created. You can now ask Mr Jones to approve it")
+        return redirect(url_for("home"))  
+
+@app.route("/admin_request_pass", methods=["GET","POST"])
+@login_required
+def admin_request_pass():
     #this page for the admin both creates and approves the request
     if request.method == "GET":
-        checkPaper()
         return render_template("request_pass.html")
     elif request.method == "POST":
         name = request.form.get("name")
@@ -372,9 +307,34 @@ def request_pass():
 
 @app.route("/request_wp", methods=["GET","POST"])
 def request_wp():
+    if request.method == "GET":
+        #checkPaper()
+        return render_template("request_wp.html")
+    elif request.method == "POST":
+        name = request.form.get("name")
+        date = datetime.strptime(request.form.get("date"), '%Y-%m-%d').date()
+        request_datetime = datetime.now()
+        new_wp_pass_request = WPPass(name=name, date=date,request_datetime=request_datetime,rejected=False)
+        db.session.add(new_wp_pass_request)
+        db.session.commit()
+        flashMessage = "Hi " + name + " your Warriors Period pass for "
+        tomorrow = datetime.today() + timedelta(days=1)
+
+        if date == datetime.today().date():
+            flashMessage += "today"
+        elif date == tomorrow.date():
+            flashMessage += "tomorrow"
+        else:
+            flashMessage += str(date)
+        flashMessage += " has been created. You can now ask Mr Jones to approve it"
+        flash(flashMessage)
+        return redirect(url_for("home")) 
+
+@app.route("/admin_request_wp", methods=["GET","POST"])
+@login_required
+def admin_request_wp():
     #this page for the admin both creates and approves the request
     if request.method == "GET":
-        checkPaper()
         return render_template("request_wp.html")
     elif request.method == "POST":
         name = request.form.get("name")
@@ -398,51 +358,10 @@ def resetdb():
         flash("The DB was just reset","error")
         return redirect(url_for("home"))
 
-@app.route("/summary")
-@login_required
-def summary():
-    approved_passes = db.session.execute(db.select(HallPass).\
-                                            filter(HallPass.approved_datetime != None)).scalars()
-        
-    approved_WP = db.session.execute(db.select(WPPass).\
-                                            filter(WPPass.approved_datetime != None)).scalars()
-    
-    printer.size = adafruit_thermal_printer.SIZE_LARGE
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.print("SUMMARY FOR")
-    printer.print(date.today().strftime("%B %d, %Y"))
-    printer.justify = adafruit_thermal_printer.JUSTIFY_LEFT
-    printer.size = adafruit_thermal_printer.SIZE_MEDIUM
-    printer.print("Hall Passes:")
-    printer.size = adafruit_thermal_printer.SIZE_SMALL
-    for p in approved_passes:
-        printer.print(p.name + " | " + p.destination)
-        goneTime = p.back_datetime - p.approved_datetime
-        printer.underline = adafruit_thermal_printer.UNDERLINE_THICK
-        printer.print(p.approved_datetime.strftime("%B %d, %y %I:%M %p") + "(" + str(int(goneTime.total_seconds() // 60)) + " mins)")
-        printer.underline = None
-
-    printer.feed(1)
-    printer.size = adafruit_thermal_printer.SIZE_MEDIUM
-    printer.print("Warrior's passes:")
-    printer.size = adafruit_thermal_printer.SIZE_SMALL
-    printer.feed(1)
-    for p in approved_WP:
-        printer.print(">" + p.name + "|" + p.date.strftime("%B %d, %Y"))
-
-    printer.size = adafruit_thermal_printer.SIZE_LARGE
-    printer.feed(1)
-    printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
-    printer.print("END SUMMARY")
-    printer.feed(2)
-    flash("Summary print successful")
-    return redirect(url_for("home"))
 
 ###
 #Initialize Printer
 ###
-printer = initializePrinter()
-printer_escpos = initializePrinter_escpos()
 
 db = SQLAlchemy(app)
 
@@ -477,5 +396,4 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(100))
 
 if __name__ == "__main__":
-    #app.run(port=443, host='0.0.0.0', debug=True,ssl_context="adhoc")
-    app.run(port=443, host='0.0.0.0', debug=False,ssl_context="adhoc")
+    app.run(host='0.0.0.0')
